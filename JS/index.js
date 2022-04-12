680d042d966c57d822a59754d49026da41254a2e
KelechiE-ART <89420505+KelechiE-ART@users.noreply.github.com>
2022-04-11 23:43:36 -0700

    dscsw

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,29 @@
 console.log("It works!");
+
+$(".logo").hover(function() {
+    $('h1').fadeOut(800);
+    $('h1').fadeIn(500);
+});
+
+
+$(".card").click(function() {
+    alert("color");
+});
+
+ScrollReveal().reveal('.Examples', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.mySkills', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.myWork', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
